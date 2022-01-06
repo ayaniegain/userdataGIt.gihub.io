7f3ea52486c7b8cc476aba27fcad3d6546b08de7
ayaniegain <ayaniegain@gmail.com>
2022-01-06 11:14:28 +0530

    done
    h

---
+++ build/precache-manifest.0faa93ebc19d8c02a2f654e6108012f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a65d510124a2e11c967225ec257b678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f7eeb0cd9ec2eca0069",
+    "url": "/static/css/main.07b738c9.chunk.css"
+  },
+  {
+    "revision": "c987414103580e7cffd3",
+    "url": "/static/js/2.b057cd99.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b057cd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7eeb0cd9ec2eca0069",
+    "url": "/static/js/main.ba94c463.chunk.js"
+  },
+  {
+    "revision": "1877e69cc92e77fffa19",
+    "url": "/static/js/runtime-main.147a4b9c.js"
+  }
+]);
